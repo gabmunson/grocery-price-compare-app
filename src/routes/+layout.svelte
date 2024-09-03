e506fe5c8c9319ac46e47596f84d31e42901173b
Gabrielle <munsongabbi@gmail.com>
2024-09-02 22:11:32 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">PriceFinder
+				</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="www.linkedin.com/in/gabrielle-munson"
+					target="_blank"
+					rel="noreferrer"
+				>
+					LinkedIn
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/gabmunson"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let isFocused: boolean = true;
+
+	let searchQuery = '';
+
+	function searchWebsites() {
+		console.log( searchQuery );
+	}
+</script>
+
+<main-container>
+	<div>
+		<h2 class="h2">Search for a grocery item:</h2>
+	</div>
+	<div class="search-bar">
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<input type="search" placeholder="Search..." bind:value={searchQuery}/>
+			<button class="variant-soft-primary" on:click={searchWebsites}><h6 class="h6">Search</h6></button>
+		</div>
+	</div>
+</main-container>
+
+<style>
+	main-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		width: 100%;
+	}
+
+	.search-bar {
+		padding: 50px;
+	}
+</style>
+
